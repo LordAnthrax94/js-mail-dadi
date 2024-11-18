348f15611f6eb50bc6d6441165e7f625a9a9428a
LordAnthrax94 <andreagi1994@gmail.com>
2024-11-18 15:51:48 +0100

    fix: aggiornato sistema mail

--- Validazione-email.js
+++ Validazione-email.js
@@ -1,5 +1,8 @@
 //Inserimento dell'email dall'utente
-//Dopo l'inserimento si comincia il gioco dei dadi
+//Comparare l'email inserita dall'utente con quelle già nell'array
+//Se l-email è valida allora si potrà accedere al gioco
+//Se l-email non è valida allora il promp continuerà a chiedere la convalida dell'e-mail
+
 
 
 const mailconvalidate = [
@@ -13,12 +16,16 @@ let permesso = false;
 
 while(!permesso){
 let inseriremail = prompt('Inserire e-mail').toLowerCase()
+let messaggio = 'Mail non valida'
 
- permesso = true;
-for(let i = 0; i < mailconvalidate.length; i++ ){
-  mailconvalidate[i] == mailconvalidate
-}
+  for(let i = 0; i < mailconvalidate.length; i++ ){
+    if(mailconvalidate[i] === inseriremail){
+      permesso = true;
+      messaggio = 'Mail Valida'
+    }    
 
+  }
+    console.log(messaggio);
 }
 
 
